fd33e30b14b9894a479cc96f42d72a518d5df9ea
Brandon Belcher <c15brandon.belcher@usafa.edu>
2013-11-05 16:46:12 -0700

    main.c and game_shell were updated to move the player across the screen with the corresponding button pushes.

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 #include "buttons/button.h"
 #include "LCD/LCD.h"
 
-
 void init_timer();
 void init_buttons();
+unsigned char player;
 /*
  * main.c
  */
 int main(void) {
 	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 
-	unsigned char player = initPlayer();
+	char *messageYou = "You";
+	char *messageWon = "Won!";
+
+	player = initPlayer();
 
 	initSPI();
 	LCDinit();
@@ -24,9 +27,13 @@ int main(void) {
 	printPlayer(player);
 
 	while (1) {
-		while (didPlayerWin(player) != 0xC7) {
+		while (!didPlayerWin(player)) {
 
 		}
+		cursorToLineOne();
+		writeString(messageYou);
+		cursorToLineTwo();
+		writeString(messageWon);
 		/*
 		 *
 		 * {
@@ -48,10 +55,31 @@ int main(void) {
 
 	return 0;
 }
+
+void movePlayerInResponseToButtonPush(char buttonToTest) {
+	clearPlayer(player);
+	player = movePlayer(player, buttonToTest);
+	printPlayer(player);
+}
+
+void testAndRespondToButtonPush(char buttonToTest) {
+	if (buttonToTest & P1IFG) {
+		if (buttonToTest & P1IES) {
+			movePlayerInResponseToButtonPush(buttonToTest);
+			//           clearTimer();
+		} else {
+			debounce();
+		}
+
+		P1IES ^= buttonToTest;
+		P1IFG &= ~buttonToTest;
+	}
+}
+
 //
 // YOUR TIMER A ISR GOES HERE
 //
-void init_timer(){
+void init_timer() {
 	// do timer initialization work
 }
 void init_buttons() {
@@ -63,4 +91,12 @@ void init_buttons() {
 	//	P1OUT |= BIT1 | BIT2 | BIT3;                   // configure as pull-up
 
 	P1IFG &= ~(BIT0 | BIT1 | BIT2 | BIT4);
+}
+
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1_ISR(void) {
+	testAndRespondToButtonPush(BIT0);
+	testAndRespondToButtonPush(BIT1);
+	testAndRespondToButtonPush(BIT2);
+	testAndRespondToButtonPush(BIT4);
 }
