15e27c6c13c83ea66073ee3a8bb1784b91b63253
Brandon Belcher <c15brandon.belcher@usafa.edu>
2013-11-07 15:10:29 -0700

    main.c was updated with the timer functionality via an interrupt. it was also updated to display game over when the 2 seconds was up for the move. Git was not working last night so there are alot of changes with this commit.

--- main.c
+++ main.c
@@ -5,7 +5,14 @@
 
 void init_timer();
 void init_buttons();
+void endOfGame();
+void clearTimer();
 unsigned char player;
+char timerCount = 0;
+char flag = 0;
+char buttons[] = { BIT1, BIT2, BIT4, BIT0 };
+
+
 /*
  * main.c
  */
@@ -14,6 +21,8 @@ int main(void) {
 
 	char *messageYou = "You";
 	char *messageWon = "Won!";
+	char *messageGame = "Game";
+	char *messageOver = "Over!";
 
 	player = initPlayer();
 
@@ -27,13 +36,27 @@ int main(void) {
 	printPlayer(player);
 
 	while (1) {
-		while (!didPlayerWin(player)) {
-
+		if (didPlayerWin(player)) {
+			__disable_interrupt();
+			clearPlayer(player);
+			cursorToLineOne();
+			writeString(messageYou);
+			cursorToLineTwo();
+			writeString(messageWon);
+			__delay_cycles(1000000);
+			endOfGame();
 		}
-		cursorToLineOne();
-		writeString(messageYou);
-		cursorToLineTwo();
-		writeString(messageWon);
+		if (timerCount >= 4) {
+			__disable_interrupt();
+			clearPlayer(player);
+			cursorToLineOne();
+			writeString(messageGame);
+			cursorToLineTwo();
+			writeString(messageOver);
+			__delay_cycles(1000000);
+			endOfGame();
+		}
+
 		/*
 		 *
 		 * {
@@ -56,6 +79,27 @@ int main(void) {
 	return 0;
 }
 
+void endOfGame(){
+	char buttonPressed = pollP1Buttons(buttons, 4);
+	char stillWaiting = 0;
+	while (stillWaiting == 0){
+		if(buttonPressed == BIT0 || buttonPressed == BIT1 || buttonPressed == BIT2 || buttonPressed == BIT4){
+			waitForP1ButtonRelease(BIT0 | BIT1 | BIT2 | BIT4);
+			LCDclear();
+			player = initPlayer();
+			printPlayer(player);
+			stillWaiting = 1;
+			__enable_interrupt();
+		}
+	}
+}
+
+void clearTimer() {
+	timerCount = 0;
+	TACTL |= TACLR;
+
+}
+
 void movePlayerInResponseToButtonPush(char buttonToTest) {
 	clearPlayer(player);
 	player = movePlayer(player, buttonToTest);
@@ -66,7 +110,8 @@ void testAndRespondToButtonPush(char buttonToTest) {
 	if (buttonToTest & P1IFG) {
 		if (buttonToTest & P1IES) {
 			movePlayerInResponseToButtonPush(buttonToTest);
-			//           clearTimer();
+			clearTimer();
+			debounce();
 		} else {
 			debounce();
 		}
@@ -76,19 +121,35 @@ void testAndRespondToButtonPush(char buttonToTest) {
 	}
 }
 
-//
-// YOUR TIMER A ISR GOES HERE
-//
-void init_timer() {
-	// do timer initialization work
+// Flag for continuous counting is TAIFG
+#pragma vector=TIMER0_A1_VECTOR
+__interrupt void TIMER0_A1_ISR() {
+	TACTL &= ~TAIFG;            // clear interrupt flag
+//	flag = 1;
+	timerCount++;
+
 }
+
+void init_timer() {
+	TACTL &= ~(MC1 | MC0);        // stop timer
+
+	TACTL |= TACLR;             // clear TAR
+
+	TACTL |= TASSEL1;   // configure for SMCLK - what's the frequency (roughly)?
+
+	TACTL |= ID1 | ID0; // divide clock by 8 - what's the frequency of interrupt?
+
+	TACTL &= ~TAIFG;            // clear interrupt flag
+
+	TACTL |= MC1;               // set count mode to continuous
+
+	TACTL |= TAIE;              // enable interrupt
+}
+
 void init_buttons() {
 	configureP1PinAsButton(BIT0 | BIT1 | BIT2 | BIT4);
 	P1IE |= BIT0 | BIT1 | BIT2 | BIT4;                 // enable the interrupts
 	P1IES |= BIT0 | BIT1 | BIT2 | BIT4; // configure interrupt to sense falling edges
-
-	//	P1REN |= BIT1 | BIT2 | BIT3;    // enable internal pull-up/pull-down network
-	//	P1OUT |= BIT1 | BIT2 | BIT3;                   // configure as pull-up
 
 	P1IFG &= ~(BIT0 | BIT1 | BIT2 | BIT4);
 }
